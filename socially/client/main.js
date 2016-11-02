02fd370196b39ede6c923446991397df28d972dd
tiga05 <tiga05@gmx.de>
2016-11-02 02:52:43 +0100

    Initial commit

---
+++ socially/client/main.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Parties } from '../imports/api/parties';
+
+import { name as Socially } from '../imports/ui/components/socially/socially';
---
+++ socially/imports/ui/components/socially/socially.js
@@ -0,0 +1,34 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import template from './socially.html';
+import { name as PartiesList } from '../partiesList/partiesList';
+//import { name as PartyDetails } from '../partyDetails/partyDetails';
+import { name as Navigation } from '../navigation/navigation';
+
+class Socially {}
+
+const name = 'socially';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter,
+  PartiesList,
+  //PartyDetails,
+  Navigation
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: Socially
+})
+  .config(config);
+
+function config($locationProvider, $urlRouterProvider) {
+  'ngInject';
+
+  $locationProvider.html5Mode(true);
+
+  $urlRouterProvider.otherwise('/parties');
+}
